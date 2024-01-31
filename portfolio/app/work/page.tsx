3ee7331067e19c58db6d956ad81012b9ac9cfb35
Coex <email.coex@gmail.com>
2024-01-31 16:23:37 +0530

    working on noble day project

--- portfolio/app/work/page.tsx
+++ portfolio/app/work/page.tsx
@@ -1,10 +1,40 @@
+"use client";
+
 import React from "react";
 import "../work.css";
+import "../header.css";
 import FullpageProviderWork from "@/components/fullpageProviderWork";
+import { Header } from "@/components/header";
+import { HeaderNavigation } from "@/components/headerNavigation";
 
 export default function WorkPage() {
+  const projectsData = [
+    {
+      title: "Sneakers Shop",
+      description: "page",
+      link: "",
+    },
+    {
+      title: "Seo Exp",
+      description: "page",
+      link: "",
+    },
+    {
+      title: "Sports Theme",
+      description: "page",
+      link: "",
+    },
+    {
+      title: "Nike",
+      description: "page",
+      link: "",
+    },
+  ];
+
   return (
     <>
+      <Header color="Dark"></Header>
+      <HeaderNavigation />
       <FullpageProviderWork>
         <div id="fullpage">
           <div className="background">
@@ -13,206 +43,46 @@ export default function WorkPage() {
             PROJECTS
           </div>
 
-          <div className="section s2 first">
-            <div className="fp-tableCell" style={{ height: 937 }}>
-              <div className="fullpage__slide">
-                <a
-                  className="image image--works image--works1 anime"
-                  href="https://dev-codemax.github.io/youtube-html-css-js-ecommerce/"
-                >
-                  <div className="image__over">
-                    <div className="image__cover">1</div>
-                    <div className="image__cover">2</div>
-                  </div>
-                  <div className="page-num anime">
-                    <p>01</p>
-                  </div>
-                </a>
-                <div className="title">
-                  <h2 className="title__text js-letter anime">
-                    Sneakers
-                    <br />
-                    SHOP
-                  </h2>
-                  <div className="js-letter anime borderv">
-                    <span></span>
-                    <span className="js-letter"></span>
-                  </div>
-                  <p className="title__lead js-letter anime">page</p>
-                  <div className="btn-wrap js-letter anime">
-                    <a
-                      className="btn"
-                      href="https://dev-codemax.github.io/youtube-html-css-js-ecommerce/"
-                    >
-                      Show me
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="section s3 second">
-            <div className="fp-tableCell" style={{ height: 937 }}>
-              <div className="fullpage__slide">
-                <a
-                  className="image image--works image--works2 anime"
-                  href="
-        https://dev-codemax.github.io/newseoweb/
-        "
-                >
-                  <div className="image__over">
-                    <div className="image__cover">1</div>
-                    <div className="image__cover">2</div>
-                  </div>
-                  <div className="page-num anime">
-                    <p>02</p>
-                  </div>
-                </a>
-                <div className="title">
-                  <h2 className="title__text js-letter anime">
-                    Seo
-                    <br />
-                    EXP
-                  </h2>
-                  <div className="js-letter anime borderv">
-                    <span></span>
-                    <span className="js-letter"></span>
-                  </div>
-                  <p className="title__lead js-letter anime">page</p>
-                  <div className="btn-wrap js-letter anime">
-                    <a
-                      className="btn"
-                      href="
-            https://dev-codemax.github.io/newseoweb/
-            "
-                    >
-                      Show me
-                    </a>
+          {projectsData.map((item, index) => (
+            <div className={`section s${index} `} key={item.title}>
+              <div className="fp-tableCell" style={{ height: 937 }}>
+                <div className="fullpage__slide">
+                  <a
+                    className={`image image--works image--works${
+                      index + 1
+                    } anime`}
+                    href={item.link}
+                  >
+                    <div className="image__over">
+                      <div className="image__cover">1</div>
+                      <div className="image__cover">2</div>
+                    </div>
+                    <div className="page-num anime">
+                      <p>0{index + 1}</p>
+                    </div>
+                  </a>
+                  <div className="title">
+                    <h2 className="title__text js-letter anime">
+                      {item.title}
+                      <br />
+                    </h2>
+                    <div className="js-letter anime borderv">
+                      <span></span>
+                      <span className="js-letter"></span>
+                    </div>
+                    <p className="title__lead js-letter anime">
+                      {item.description}
+                    </p>
+                    <div className="btn-wrap js-letter anime">
+                      <a className="btn" href={item.link}>
+                        Show me
+                      </a>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div className="section s4 third">
-            <div className="fp-tableCell" style={{ height: 937 }}>
-              <div className="fullpage__slide">
-                <a
-                  className="image image--works image--works3 anime"
-                  href="https://dev-codemax.github.io/Sports-Theme-Landing-Page-Concept-master/"
-                >
-                  <div className="image__over">
-                    <div className="image__cover">1</div>
-                    <div className="image__cover">2</div>
-                  </div>
-                  <div className="page-num anime">
-                    <p>03</p>
-                  </div>
-                </a>
-                <div className="title">
-                  <h2 className="title__text js-letter anime">
-                    Sports
-                    <br />
-                    THEME
-                  </h2>
-                  <div className="js-letter anime borderv">
-                    <span></span>
-                    <span className="js-letter"></span>
-                  </div>
-                  <p className="title__lead js-letter anime">page</p>
-                  <div className="btn-wrap js-letter anime">
-                    <a
-                      className="btn"
-                      href="https://dev-codemax.github.io/Sports-Theme-Landing-Page-Concept-master/"
-                    >
-                      Show me
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div className="section s5 fourth">
-            <div className="fp-tableCell" style={{ height: 937 }}>
-              <div className="fullpage__slide">
-                <a
-                  className="image image--works image--works4 anime"
-                  href="
-        https://dev-codemax.github.io/NIKE/"
-                >
-                  <div className="image__over">
-                    <div className="image__cover">1</div>
-                    <div className="image__cover">2</div>
-                  </div>
-                  <div className="page-num anime">
-                    <p>04</p>
-                  </div>
-                </a>
-                <div className="title">
-                  <h2 className="title__text js-letter anime">
-                    Iphone 12
-                    <br />
-                    PROMAX
-                  </h2>
-                  <div className="js-letter anime borderv">
-                    <span></span>
-                    <span className="js-letter"></span>
-                  </div>
-                  <p className="title__lead js-letter anime">page</p>
-                  <div className="btn-wrap js-letter anime">
-                    <a
-                      className="btn"
-                      href="
-            https://dev-codemax.github.io/NIKE/"
-                    >
-                      Show me
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div className="section s6 fifth">
-            <div className="fp-tableCell" style={{ height: 937 }}>
-              <div className="fullpage__slide">
-                <a
-                  className="image image--works image--works5 anime"
-                  href="
-        https://codemax.info/"
-                >
-                  <div className="image__over">
-                    <div className="image__cover">1</div>
-                    <div className="image__cover">2</div>
-                  </div>
-                  <div className="page-num anime">
-                    <p>05</p>
-                  </div>
-                </a>
-                <div className="title anime">
-                  <h2 className="title__text js-letter">
-                    My
-                    <br />
-                    PORTFOLIO
-                  </h2>
-                  <div className="js-letter borderv">
-                    <span></span>
-                    <span className="js-letter"></span>
-                  </div>
-                  <p className="title__lead js-letter anime">website</p>
-                  <div className="btn-wrap js-letter anime">
-                    <a
-                      className="btn"
-                      href="
-            https://codemax.info/"
-                    >
-                      Show me
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+          ))}
         </div>
       </FullpageProviderWork>
     </>
