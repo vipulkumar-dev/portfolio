1c2c6e1ab7a09bed0236d6013ba918492e5fc64d
Coex <email.coex@gmail.com>
2024-04-27 11:52:30 +0530

    Image replaced

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { gsap } from "gsap";
 import { CustomEase } from "gsap/CustomEase";
 gsap.registerPlugin(CustomEase);
 
-// const dM_Sans = DM_Sans({ subsets: ["latin-ext"] });
+const dM_Sans = DM_Sans({ subsets: ["latin-ext"] });
 
 export const metadata: Metadata = {
   title: "Vipul Kumar • Freelance Designer & Developer",
@@ -31,9 +31,13 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap"
           rel="stylesheet"
         ></link>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, shrink-to-fit=no"
+        ></meta>
       </head>
 
-      <body>
+      <body className={dM_Sans.className}>
         <StoreProvider>{children}</StoreProvider>
       </body>
     </html>
