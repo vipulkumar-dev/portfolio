662f838089696aaf1ec88906fd910fb3b024722e
Vipul Kumar <email.coex@gmail.com>
2024-08-31 16:45:57 +0530

    abc

--- app/GoogleAnalytics.tsx
+++ app/GoogleAnalytics.tsx
@@ -8,17 +8,17 @@ const GoogleAnalytics = () => {
     <>
       <Script
         strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_MEASUREMENT_ID}`}
+        src={`https://www.googletagmanager.com/gtag/js?id=G-32FBS7Y85P`}
       />
 
       <Script id="" strategy="lazyOnload">
         {`
+              
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', '${process.env.NEXT_PUBLIC_MEASUREMENT_ID}', {
-              page_path: window.location.pathname,
-              });
+
+              gtag('config', 'G-32FBS7Y85P');
           `}
       </Script>
     </>
