637df19cf1c40c3b91d3e96c29fd6c2c07b9d2e2
Coex <email.coex@gmail.com>
2024-09-12 10:33:36 +0530

    110%

--- components/heroSection/heroSection.tsx
+++ components/heroSection/heroSection.tsx
@@ -12,7 +12,7 @@ export function HeroSection({}) {
       <Header color="Light" />
       <HeroWrapper />
 
-      <div className="absolute left-0 top-0 z-10 h-full w-full origin-top scale-[120%]">
+      <div className="absolute left-0 top-0 z-10 h-full w-full origin-top scale-[108%]">
         <Spline scene="https://prod.spline.design/u1VFgkuq6dbrbxHS/scene.splinecode" />
       </div>
     </section>
